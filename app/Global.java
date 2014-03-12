55630246d7f0097de4ace1cbc6d694274d1b3637
Toshiyuki Ando <couger@kt.rim.or.jp>
2014-03-13 00:23:27 +0900

    APIから取得したデータを使ってメニューを表示するように

--- app/Global.java
+++ app/Global.java
@@ -1,12 +1,16 @@
 
+import com.avaje.ebean.Ebean;
+
 import java.math.BigDecimal;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import models.DailyMenu;
 import models.DailyMenuItem;
 import models.MenuItem;
 import play.Application;
 import play.GlobalSettings;
+import play.libs.Yaml;
 import play.Play;
 
 public class Global extends GlobalSettings  {
@@ -18,27 +22,14 @@ public class Global extends GlobalSettings  {
             return;
         }
 
-        if(MenuItem.find.findRowCount() == 0) {
-            MenuItem item = new MenuItem();
-            item.category = "bento";
-            item.shop_name = "MooMoo Shop";
-            item.name = "MooMoo Pizza";
-            item.price_on_order = new BigDecimal("2130");
-            item.item_image_path = "/data/lunch/images/1-1.png";
-            item.save();
-
-            DailyMenu menu = new DailyMenu();
-            menu.menu_date = new GregorianCalendar(2014, 2 - 1, 1).getTime();
-            menu.status = "open";
-
-            DailyMenuItem dMenuItem = new DailyMenuItem();
-            dMenuItem.menuItem = item;
-
-            menu.detailItems.add(dMenuItem);
-
-            menu.save();
-
+        if(Play.isDev()) {
+            if(MenuItem.find.findRowCount() == 0) {
+                Ebean.save((List) Yaml.load("fixtures/dev/menu_item.yml"));
+                Ebean.save((List) Yaml.load("fixtures/dev/daily_menu.yml"));
+                Ebean.save((List) Yaml.load("fixtures/dev/daily_menu_item.yml"));
+            }
         }
+
     }
 
 }
--- ui/app/app.js
+++ ui/app/app.js
@@ -27,6 +27,14 @@ app.controller('SigninController', ['$scope', '$location', function($scope, $loc
         };
     }])
     .controller('OrderController', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
+        $http.get('/api/daily_menus').success(function(data) {
+            $scope.daily_menus = data;
+
+            angular.forEach($scope.daily_menus, function(daily_menu) {
+                daily_menu.menu_date = jQuery.format.date(new Date(daily_menu.menu_date), 'yyyy/MM/dd (ddd)');
+            });
+        });
+
         $scope.showSideDishes = function() {
             $modal.open({
                   templateUrl: 'views/side-dishes'
