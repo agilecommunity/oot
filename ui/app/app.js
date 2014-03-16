3659a854ec2da138de953c67c6ada3968f8b9ba1
Toshiyuki Ando <couger@kt.rim.or.jp>
2014-03-16 18:11:29 +0900

    認証に成功したら、画面が遷移するように

--- ui/app/app.js
+++ ui/app/app.js
@@ -1,12 +1,40 @@
 'use strict';
 
+var transform = function(data){
+    return jQuery.param(data);
+}
+
 angular.module('OotServices', [])
     .factory('User', ['$http', '$rootScope', function($http, $rootScope) {
+
+        $rootScope.current_user = null;
+
         return {
-            login: function() {
-                alert("login");
-                }
-            };
+
+            is_signed_in: function() {
+                return !($rootScope.current_user === null);
+            }
+
+          , login: function(username, password, success, error) {
+                var parameter = {};
+                parameter.username = username;
+                parameter.password = password;
+
+                $http({
+                      method: 'POST'
+                    , url: '/authenticate/userpass'
+                    , headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+                    , transformRequest: transform
+                    , data: parameter
+                })
+                .success(function(data, status, header){
+                    success();
+                })
+                .error(function(data, status, header){
+                    error(status);
+                });
+            }
+        };
     }]);
 
 var app = angular.module('oot', [
@@ -34,11 +62,17 @@ app.config(['$routeProvider',
 
 app.controller('SigninController', ['$scope', '$location', 'User', function($scope, $location, User) {
         $scope.signin = function() {
-            User.login();
-            $location.path("/order");
+            User.login($scope.user.email, $scope.user.password
+                , function(){
+                    $location.path("/order");
+                }
+                , function(status){
+                    alert("login error status:" + status);
+                });
         };
     }])
     .controller('OrderController', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
+
         $http.get('/api/daily_menus').success(function(data) {
             $scope.daily_menus = data;
 
@@ -53,4 +87,3 @@ app.controller('SigninController', ['$scope', '$location', 'User', function($sco
             });
         };
     }]);
-
