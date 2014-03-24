b94e27479f2033c2924350ca9fb4b7663ef40e6b
Toshiyuki Ando <couger@kt.rim.or.jp>
2014-03-24 23:26:59 +0900

    PostgreSQLでも使えるコメントアウトの方法に変更

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,41 +2,41 @@
 
 # --- !Ups
 
-// メニューに出す品
+-- メニューに出す品
 create table menu_item (
-  id                     bigint not null,       // --- ID
-  category               varchar(10) not null,  // --- カテゴリ (bento=弁当, side=デザートやサラダ)
-  shop_name              varchar(255) not null, // --- 店名
-  name                   varchar(255) not null, // --- 名前
-  price_on_order         decimal(10) not null,  // --- 注文価格
-  item_image_path        varchar(1024),         // --- 画像のファイルパス
+  id                     bigint not null,       -- --- ID
+  category               varchar(10) not null,  -- --- カテゴリ (bento=弁当, side=デザートやサラダ)
+  shop_name              varchar(255) not null, -- --- 店名
+  name                   varchar(255) not null, -- --- 名前
+  price_on_order         decimal(10) not null,  -- --- 注文価格
+  item_image_path        varchar(1024),         -- --- 画像のファイルパス
 
-  created_at             timestamp,        // --- 作成日
-  updated_at             timestamp,        // --- 最終更新日
+  created_at             timestamp,        -- --- 作成日
+  updated_at             timestamp,        -- --- 最終更新日
 
   constraint pk_menu_item primary key (id)
 );
 
-// 日々のメニュー
+-- 日々のメニュー
 create table daily_menu (
-  id                     bigint not null,  // --- ID
-  menu_date              date not null,    // --- 日付
-  status                 varchar(10) default 'prepared',  // --- ステータス: prepared, open, closed
+  id                     bigint not null,  -- --- ID
+  menu_date              date not null,    -- --- 日付
+  status                 varchar(10) default 'prepared',  -- --- ステータス: prepared, open, closed
 
-  created_at             timestamp,        // --- 作成日
-  updated_at             timestamp,        // --- 最終更新日
+  created_at             timestamp,        -- --- 作成日
+  updated_at             timestamp,        -- --- 最終更新日
 
   constraint pk_daily_menu primary key (id)
 );
 
-// 日々のメニューで注文できる品
+-- 日々のメニューで注文できる品
 create table daily_menu_item (
-  id               bigint not null,  // --- ID
-  daily_menu_id    bigint not null,  // --- メニューID
-  menu_item_id     bigint not null,  // --- 弁当ID
+  id               bigint not null,  -- --- ID
+  daily_menu_id    bigint not null,  -- --- メニューID
+  menu_item_id     bigint not null,  -- --- 弁当ID
 
-  created_at             timestamp,        // --- 作成日
-  updated_at             timestamp,        // --- 最終更新日
+  created_at             timestamp,        -- --- 作成日
+  updated_at             timestamp,        -- --- 最終更新日
 
   constraint pk_daily_menu_item primary key (id)
 );
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,31 +2,31 @@
 
 # --- !Ups
 
-// ローカルユーザ
+-- ローカルユーザ
 create table local_user (
-  id                     varchar(255) not null, // --- ID
-  provider               varchar(255) not null, // --- プロバイダ
-  email                  varchar(255) not null, // --- メールアドレス
-  password               varchar(255) not null, // --- ハッシュ化されたパスワード
-  first_name             varchar(255) not null, // --- 姓
-  last_name              varchar(255) not null, // --- 名
-  is_admin               boolean default false, // --- 管理者かどうか
+  id                     varchar(255) not null, -- --- ID
+  provider               varchar(255) not null, -- --- プロバイダ
+  email                  varchar(255) not null, -- --- メールアドレス
+  password               varchar(255) not null, -- --- ハッシュ化されたパスワード
+  first_name             varchar(255) not null, -- --- 姓
+  last_name              varchar(255) not null, -- --- 名
+  is_admin               boolean default false, -- --- 管理者かどうか
 
-  created_at             timestamp,        // --- 作成日
-  updated_at             timestamp,        // --- 最終更新日
+  created_at             timestamp,        -- --- 作成日
+  updated_at             timestamp,        -- --- 最終更新日
 
   constraint pk_local_user primary key (id)
 );
 
-// ローカルトークン
+-- ローカルトークン
 create table local_token (
-  uuid                   varchar(255) not null, // --- プロバイダ
-  email                  varchar(255) not null, // --- メールアドレス
+  uuid                   varchar(255) not null, -- --- プロバイダ
+  email                  varchar(255) not null, -- --- メールアドレス
 
-  created_at             timestamp,             // --- 作成日時
-  expire_at              timestamp,             // --- 廃棄日時
+  created_at             timestamp,             -- --- 作成日時
+  expire_at              timestamp,             -- --- 廃棄日時
 
-  is_sign_up             boolean default false, // --- サインアップ中か?
+  is_sign_up             boolean default false, -- --- サインアップ中か?
 
   constraint pk_local_token primary key (uuid)
 );
