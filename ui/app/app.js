b6da4ccafcd63c86848e6a11713f6ee1dfd7b025
Toshiyuki Ando <couger@kt.rim.or.jp>
2014-03-30 22:15:51 +0900

    refs #1 管理者の場合はサインイン後、管理インデックスに移動するように

--- ui/app/app.js
+++ ui/app/app.js
@@ -307,7 +307,11 @@ app.controller('SigninController', ['$scope', '$location', 'User', function($sco
         $scope.signin = function() {
             User.signin($scope.user_email, $scope.user_password, {
                   success: function(){
-                    $location.path("/order");
+                    var path = "/order";
+                    if (User.current_user().is_admin === true) { // 管理者の場合は管理インデックスに飛ばす
+                        path = "/admin/index";
+                    }
+                    $location.path(path);
                 }
                 , error: function(status){
                     alert("サインインに失敗しました。 status:" + status);
