615e01773fbd30ec3767e4bdb39706e22f2b749a
Toshiyuki Ando <couger@kt.rim.or.jp>
2014-03-31 23:13:49 +0900

    fixes #9 日付をUnix時間からDateに変換していなかった問題を修正

--- ui/app/app.js
+++ ui/app/app.js
@@ -94,6 +94,15 @@ angular.module('OotServices', ['ngResource', 'ngRoute'])
     }])
     .factory('DailyMenu', ['$resource', function($resource){  // 日々のメニューを扱うサービス
 
+        var transformResponse = function(data, headersGetter){  // 結果を変換したい場合はtransformResponseを使う
+            // 日付が数字でくるとDateに変換されないので、こちらで変換する
+            var list = angular.fromJson(data);
+            angular.forEach(list, function(item) {
+                item.menu_date = new Date(item.menu_date);
+            });
+            return list;
+        };
+
         var DailyMenu =                                // ここだけ丁寧に解説
             $resource(                                 // RESTのAPIを簡単に扱える$resourceサービスを利用する
                   '/api/daily-menus/:id'               // APIのURL。:idは変数 query,createなど必要のないときは使われない
@@ -102,20 +111,14 @@ angular.module('OotServices', ['ngResource', 'ngRoute'])
                       query: {                         // queryはオブジェクト全件を取り出す
                           method: "GET"
                         , isArray: true                    // 結果が配列になる場合は必ずtrueにする(でないと、エラーが発生する)
-                        , transformResponse: function(data, headersGetter){  // 結果を変換したい場合はtransformResponseを使う
-                            // 日付が数字でくるとDateに変換されないので、こちらで変換する
-                            var list = angular.fromJson(data);
-                            angular.forEach(list, function(item) {
-                                item.menu_date = new Date(item.menu_date);
-                            });
-                            return list;
-                        }
+                        , transformResponse: transformResponse
                       }
                     , queryByStatus: {
                           method: "GET"
                         , url: "/api/daily-menus/status/:status"
                         , params: {status: "@status"}
                         , isArray: true
+                        , transformResponse: transformResponse
                     }
                     , getByMenuDate: {
                           method: "GET"
