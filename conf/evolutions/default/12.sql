4adcf11e48518569fc3d4455997a950dbb189809
Toshiyuki Ando <couger@kt.rim.or.jp>
2015-02-15 00:25:56 +0900

    PostgreSQLでEvolutionがエラーになっていた問題を修正 #71

--- conf/evolutions/default/12.sql
+++ conf/evolutions/default/12.sql
@@ -3,9 +3,13 @@
 ALTER TABLE menu_item ADD discount_on_order decimal(10) not null default 0; -- --- 注文時の割引額
 UPDATE menu_item SET discount_on_order = 0;
 
-ALTER TABLE menu_item ALTER COLUMN price_on_order RENAME TO fixed_on_order; -- 注文時の定価
+ALTER TABLE menu_item ADD fixed_on_order decimal(10) not null default 0; -- --- 注文時の定価
+UPDATE menu_item set fixed_on_order = price_on_order;
+ALTER TABLE menu_item DROP price_on_order;
 
 # --- !Downs
 
 ALTER TABLE menu_item DROP discount_on_order;
-ALTER TABLE menu_item ALTER COLUMN fixed_on_order RENAME TO price_on_order;
+ALTER TABLE menu_item ADD price_on_order decimal(10) not null default 0; -- --- 注文時の定価
+UPDATE menu_item set price_on_order = fixed_on_order;
+ALTER TABLE menu_item DROP fixed_on_order;
